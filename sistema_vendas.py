faa6ea31a26d94fa7f27946be9187b1e65f84a98
RomarioFilipeS <rofilipe113@gmail.com>
2026-07-02 21:12:39 -0300

    Opcao 03

--- sistema_vendas.py
+++ sistema_vendas.py
@@ -1,4 +1,4 @@
-estoque = {
+estoque_produto = {
     1 : {"Nome": "Tenis Nike", "preco": 400.00,"Quantidade": 2 },
     2 : {"Nome":"Tenis Adidas", "preco": 700.00,"Quantidade": 100},
     3 : {"Nome": "Camisa CazeTV", "preco": 80.00,"Quantidade": 8 },
@@ -21,20 +21,42 @@ while True:
     if opcao == 1:
         print("Visualizar Estoque!")
         print("ID | Nome | VALOR | Quantidade")
-        for c,v in estoque.items():
+        for c,v in estoque_produto.items():
             print(f"{c} | {v['Nome']} | {v['preco']} | {v['Quantidade']}")
     elif opcao == 2:
         print("Adicionar item ao carrinho!")
         id_produto = int(input("Qual id do produto deseja compra? "))
-        if id_produto in estoque:
+        if id_produto in estoque_produto:
             qtd_produto = int(input("Qual quantidade do produto deseja compra? "))
             if qtd_produto <= 0:
                 print("Quantidade Invalida!")
-            elif qtd_produto <= estoque[id_produto]['Quantidade']:
+            elif qtd_produto <= estoque_produto[id_produto]['Quantidade']:
+                item = {
+                    "Qtd" : qtd_produto,
+                    "Nome" : estoque_produto[id_produto]['Nome'],
+                    "preco" : estoque_produto[id_produto]['preco'],
+                    "Valor_total": qtd_produto * estoque_produto[id_produto]['preco'],
+                }
+                carrinho.append(item)
+                estoque_produto[id_produto]['Quantidade'] -= qtd_produto
+            else :
+                print(f"Quantidade Indisponivel, temos apenas {estoque_produto[id_produto]['Quantidade']} no estoque.")
+        else :
+                print("ID informado nao existe no estoque."
+              )
     elif opcao == 3:
-        print("Visualizar Carrinhos!")
+        if carrinho:
+            print("Visualizar Carrinhos!")
+            subtotal = 0
+            for i in carrinho:
+                print(f"{i["Qtd"]}x {i['Nome']} no valor de R$ {i['preco']}(cada)\nTotal R$ {i['Valor_total']}")
+                subtotal += i['Valor_total']
+            print (f"Total R$ {subtotal}")
+        else :
+            print("Carrinho vazio")
     elif opcao == 4:
         print("Finalizar Compra!")
+
     elif opcao == 0:
         print("Sair do sistema.")
         break
